52b400d9e2b0f814569a249fe04af2c47d010cc5
DFMERA <df.mera@gmail.com>
2019-12-31 14:22:09 -0500

    first commit

---
+++ ImageClassification.Predict/Model/ModelScorer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using ImageClassification.DataModels;
+using System.IO;
+using Microsoft.ML;
+using System.Collections.Generic;
+using Microsoft.ML.Data;
+
+namespace ImageClassification.Model
+{
+    public class ModelScorer
+    {
+        private readonly string imagesFolder;
+        private readonly string modelLocation;
+        private readonly MLContext mlContext;
+
+        public ModelScorer(string imagesFolder, string modelLocation)
+        {
+            this.imagesFolder = imagesFolder;
+            this.modelLocation = modelLocation;
+            mlContext = new MLContext(seed: 1);
+        }
+
+        public ImagePrediction ClassifyImages()
+        {
+            
+            // Load the model
+            ITransformer loadedModel = mlContext.Model.Load(modelLocation,out var modelInputSchema);
+
+            // Make prediction engine (input = ImageDataForScoring, output = ImagePrediction)
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ImageData, ImagePrediction>(loadedModel);
+
+            IEnumerable<ImageData> imagesToPredict = LoadImagesFromDirectory(imagesFolder, true);
+
+            //Predict the first image in the folder
+            //
+            ImageData imageToPredict = new ImageData
+            {
+                ImagePath = imagesToPredict.First().ImagePath
+            };
+
+            var prediction = predictionEngine.Predict(imageToPredict);
+
+            return prediction;
+            //Console.WriteLine($"ImageFile : [{Path.GetFileName(imageToPredict.ImagePath)}], " +
+            //                  $"Scores : [{string.Join(",", prediction.Score)}], " +
+            //                  $"Predicted Label : {prediction.PredictedLabelValue}");
+                      
+            //////
+
+            
+        }
+
+        public ImagePrediction ClassifySingleImage()
+        {
+
+            // Load the model
+            ITransformer loadedModel = mlContext.Model.Load(modelLocation, out var modelInputSchema);
+
+            // Make prediction engine (input = ImageDataForScoring, output = ImagePrediction)
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<ImageData, ImagePrediction>(loadedModel);
+
+            //Predict the image loaded in form
+            //
+            ImageData imageToPredict = new ImageData
+            {
+                ImagePath = imagesFolder
+            };
+
+            var prediction = predictionEngine.Predict(imageToPredict);
+
+            return prediction;
+            
+        }
+
+        public static IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameasLabel = true)
+        {
+            var files = Directory.GetFiles(folder, "*",
+                searchOption: SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                if ((Path.GetExtension(file) != ".jpg") && (Path.GetExtension(file) != ".png"))
+                    continue;
+
+                var label = Path.GetFileName(file);
+                if (useFolderNameasLabel)
+                    label = Directory.GetParent(file).Name;
+                else
+                {
+                    for (int index = 0; index < label.Length; index++)
+                    {
+                        if (!char.IsLetter(label[index]))
+                        {
+                            label = label.Substring(0, index);
+                            break;
+                        }
+                    }
+                }
+
+                yield return new ImageData()
+                {
+                    ImagePath = file,
+                    Label = label
+                };
+
+            }
+        }
+    }
+}
---
+++ ImageClassification.PredictWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageClassification.DataModels;
+using ImageClassification.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ImageClassification.PredictWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Message { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public IFormFile ImageFile { get; set; }
+        public ImagePrediction Predict { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = ".";
+        }
+
+        public async Task OnPostAsync()
+        {
+            Message = "LOADING...";
+
+
+            string assetsRelativePath = @"../../../assets";
+            string assetsPath = GetAbsolutePath(assetsRelativePath);
+
+
+            var imagesFolder = Path.Combine(assetsPath, "inputs", "images-for-predictions", "tmp-web-image");
+            var imageClassifierZip = Path.Combine(assetsPath, "inputs", "MLNETModel", "imageClassifier.zip");
+            imagesFolder += @"/" + ImageFile.FileName;
+
+            try
+            {
+                using (var stream = System.IO.File.Create(imagesFolder))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+
+
+                var modelScorer = new ModelScorer(imagesFolder, imageClassifierZip);
+                Predict = modelScorer.ClassifySingleImage();
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+
+            Message = "...";
+            //return RedirectToPage("./Index");
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+    }
+}
